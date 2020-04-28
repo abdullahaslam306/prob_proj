d59fa4dc7094d65d61dcc4f4641370595961cc12
abdullahaslam306 <f178135@nu.edu.pk>
2020-04-28 16:48:37 +0500

    hassan chok fr

---
+++ charts.php
@@ -0,0 +1,38 @@
+<?php include("header.php"); ?>
+<div class="container">
+	<h2> Charts</h2>
+	<div class="row">
+		<div class="col-md2"></div>
+		 <div class="form-group col-md-4">
+      <label for="inputState">Select Country</label>
+      <select id="inputState" class="form-control" >
+        <option selected>Choose...</option>
+        <option value="">Pakistan</option>
+        <option value="">China</option>
+        <option value="">America</option>
+        <option value="">India</option>
+         <option value="">Hassan ka Ghr</option>
+      </select>
+    </div>
+     <div class="form-group col-md-4">
+      <label for="inputState">Select Disease</label>
+      <select id="inputState" class="form-control">
+        <option selected>Choose...</option>
+        <option value="">...</option>
+      </select>
+    </div>
+    <div class="col-md-2"></div>
+
+	</div>
+<div class="row">
+	<div class="col-md-4">
+		
+	</div>
+	<div class="col-md-8">
+	
+	</div>
+	<div class="col-md-4"></div>
+</div>
+
+</div>
+<?php include("footer.php"); ?>
---
+++ index.php
@@ -0,0 +1,13 @@
+<?php include("header.php"); ?>
+
+<div > 
+<div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-4 head1">Death Predictor</h1>
+    <p class="lead myp">by HS,HS and AA <br>
+This article was first published in 2020. It was last revised in April 2020.</p>
+  </div>
+</div>
+</div>
+
+<?php include("footer.php"); ?>
